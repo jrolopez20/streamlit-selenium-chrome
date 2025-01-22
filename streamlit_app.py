72cd7f070cfd9e92428a49b79ff773836884c6bf
Javier Rodriguez <49592840+jrolopez20@users.noreply.github.com>
2025-01-22 23:11:52 +0000

    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,95 @@
 import streamlit as st
+# Import necessary libraries
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
 
 """
-## Web scraping on Streamlit Cloud with Selenium
+# Vehicle recommendation
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
-
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-yeap Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+yeap
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+# Set up Splinter
+browser = Browser('chrome')
+# Set up base url
+base_url = "https://www.facebook.com/marketplace/toronto/search?"
+# Set up search parameters
+min_price = 1000
+max_price = 30000
+days_listed = 7
+min_mileage = 50000
+max_mileage = 200000
+min_year = 2000
+max_year = 2020
+transmission = "automatic"
+make = "Honda"
+model = "Civic"
+# Set up full url
+url = f"{base_url}minPrice={min_price}&maxPrice={max_price}&daysSinceListed={days_listed}&maxMileage={max_mileage}&maxYear={max_year}&minMileage={min_mileage}&minYear={min_year}&transmissionType={transmission}&query={make}{model}&exact=false"
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+# Visit the website
+browser.visit(url)
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+if browser.is_element_present_by_css('div[aria-label="Close"]', wait_time=10):
+    # Click on the element once it's found
+    browser.find_by_css('div[aria-label="Close"]').first.click()
 
-    driver = get_driver()
-    driver.get("http://example.com")
+# Scroll down to load more results
 
-    st.code(driver.page_source)
+# Define the number of times to scroll the page
+scroll_count = 4
+
+# Define the delay (in seconds) between each scroll
+scroll_delay = 2
+
+# Loop to perform scrolling
+for _ in range(scroll_count):
+    # Execute JavaScript to scroll to the bottom of the page
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+    # Pause for a moment to allow the content to load
+    time.sleep(scroll_delay)
+
+# Parse the HTML
+html = browser.html
+
+# Create a BeautifulSoup object from the scraped HTML
+market_soup = soup(html, 'html.parser')
+
+# Check if HTML was scraped correctly
+st.write(market_soup)
+
+# End the automated browsing session
+browser.quit()
+
+# Extract all the necessary info and insert into lists
+titles_div = market_soup.find_all('span', class_="x1lliihq x6ikm8r x10wlt62 x1n2onr6")
+titles_list = [title.text.strip() for title in titles_div]
+prices_div = market_soup.find_all('span', class_="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x676frb x1lkfr7t x1lbecb7 x1s688f xzsf02u")
+prices_list = [price.text.strip() for price in prices_div]
+mileage_div = market_soup.find_all('span', class_="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x1nxh6w3 x1sibtaa xo1l8bm xi81zsa")
+mileage_list = [mileage.text.strip() for mileage in mileage_div]
+urls_div = market_soup.find_all('a', class_="x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lku1pv")
+urls_list = [url.get('href') for url in urls_div]
+
+# Create a regular expression pattern to match city and state entries like "City, State"
+pattern = re.compile(r'(\w+(?:-\w+)?, [A-Z]{2})')
+
+# Initialize an empty list to store adjusted mileage entries
+mileage_list2 = []
+
+# Iterate through the original mileage entries
+for item in mileage_list:
+    # Append the current mileage entry to the adjusted list
+    mileage_list2.append(item)
+    
+    # Check if the current mileage entry matches the pattern and there are at least two entries in the adjusted list
+    if pattern.match(item) and len(mileage_list2) >= 2 and pattern.match(mileage_list2[-2]):
+        # If the conditions are met, insert "0K km" in between the two consecutive city and state entries
+        mileage_list2.insert(-1, '0K km')
+
+st.write(mileage_list2)
